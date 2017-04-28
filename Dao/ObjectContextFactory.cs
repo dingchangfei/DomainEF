6470c2708d20878faf85718e444a8ef5b649408b
Changfei.Ding <changfei.ding@pfizer.com>
2017-04-28 17:41:51 +0800

    add factory

---
+++ Dao/ObjectContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Objects;
+using System.Runtime.Remoting.Messaging;
+
+namespace Dao
+{
+    public class ObjectContextFactory
+    {
+        public static ObjectContext GetCurrentObjectContext()
+        {
+            ObjectContext objectContext = CallContext.GetData(typeof(ObjectContextFactory).FullName) as ObjectContext;
+
+            if (objectContext == null)
+            {
+                objectContext = new ModelContainer();
+            }
+        }
+    }
+}
